700a91cf69abfd51979d88293c737575a8e159d1
Oshadi <iboshadi@gmail.com>
2025-05-15 09:52:57 +0530

    7

--- api/index.ts
+++ api/index.ts
@@ -5,19 +5,8 @@ const { connectToDatabase } = require("../config/db");
 const { authenticateToken } = require("../middleware/authMiddleware");
 const authController = require("../controllers/authController");
 
-// app.get("/", async (req, res) => {
-//   try {
-//     const pool = await connectToDatabase();
-//     const result = await pool.request().query("SELECT GETDATE() AS currentTime");
-//     res.send(`Connected to DB. Server time: ${result.recordset[0].currentTime}`);
-//   } catch (err) {
-//     console.error("DB error:", err);
-//     res.status(500).send("Database connection failed.");
-//   }
-// });
-
 app.get("/", (req, res) => res.send("Hello Vercel"));
-app.get("/time", authController.getServerTime);
+app.get("/login", authController.getServerTime);
 
 // app.listen(3000, () => console.log("Server ready on port 3000."));
 
