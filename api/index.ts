bf068c4b37104fd97cc7888437f58d05b313b8ec
Oshadi <iboshadi@gmail.com>
2025-05-15 09:09:01 +0530

    3

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,8 @@ const app = express();
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/hello", (req, res) => res.send("Hello Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+// module.exports = app;
+
+export default app;
